83a232c2a82a7c622d2ec61dae79c39c3b71091d
dyf <1833928261@qq.com>
2020-07-16 22:00:38 +0800

    界面优化

--- comment_sentiment_analysis.py
+++ comment_sentiment_analysis.py
@@ -115,23 +115,24 @@ tag_comments_neg = load_tag_comments_neg()
 LDA_coherence = load_LDA_coherence()
 
 st.sidebar.markdown('# 项目目录')
-section = st.sidebar.radio("请选择需要展示的项目模块：", ('0. 项目介绍与规划', '1. 数据采集与抽取', '2. 数据预处理与探索', '3. 自训练情感分析模型', '4. 评论分词与改进',
-                                             '5. 词云与语义网络构建', '6. LDA主题模型构建', '7. 交互诊断与反馈'))
+section = st.sidebar.radio("请选择需要展示的项目模块：", ('0⃣️ 项目介绍与规划', '1⃣️ 数据采集与抽取', '2⃣️ 数据预处理与探索', '3⃣️ 自训练情感分析模型', '4⃣️ 评论分词与改进',
+                                             '5⃣️ 词云与语义网络构建', '6⃣️ LDA主题模型构建', '7⃣️ 交互诊断与反馈'))
 
 tag = None
-if section in ['5. 词云与语义网络构建', '6. LDA主题模型构建', '7. 交互诊断与反馈']:
+if section in ['5⃣️ 词云与语义网络构建', '6⃣️ LDA主题模型构建', '7⃣️ 交互诊断与反馈']:
     st.sidebar.markdown('# 评论标签')
     tag = st.sidebar.selectbox("  请选择分析的评论标签：",
                                ('总体评论', '外形外观', '恒温效果', '噪音大小', '出水速度', '安装服务', '耗能情况', '加热速度', '洗浴时间', '其他特色'))
 
 topic_number = 5
-if section in ['6. LDA主题模型构建']:
+if section in ['6⃣️ LDA主题模型构建']:
     st.sidebar.markdown('# LDA主题数')
     topic_number = st.sidebar.slider('  请选择需要训练的LDA主题数:', 3, 9, 5)
 
-if section == '0. 项目介绍与规划':
+if section == '0⃣️ 项目介绍与规划':
     st.markdown('## 0. 项目介绍与规划')
 
+    st.markdown('***')
     st.markdown('### 0.1 开发人员清单与分工')
     st.markdown('- 丁一凡：1、 数据爬取与数据抽取')
     st.markdown('&emsp;' * 20 + '2、数据预处理以及标签评论的处理')
@@ -148,6 +149,7 @@ if section == '0. 项目介绍与规划':
     st.markdown('&emsp;' * 15 + '3、将项目搭建在Heroku云服务器上')
     st.markdown('&emsp;' * 15 + '4、完成项目展示报告，项目立项书等')
 
+    st.markdown('***')
     st.markdown('### 0.2 项目开发环境清单')
     environment_list = pd.DataFrame(
         {'名称': ['Windows10 & MacBook Pro', 'pycharm + google colab', 'python3.7', 'MongoDB', 'GitHub', 'Heroku'],
@@ -155,14 +157,17 @@ if section == '0. 项目介绍与规划':
          })
     st.table(environment_list)
 
+    st.markdown('***')
     st.markdown('### 0.3 项目开发周期表')
     image = load_image('data/source/develop_schedule.png')
     st.image(image, use_column_width=True)
 
-    st.markdown('### 0.3 项目开发流程图')
+    st.markdown('***')
+    st.markdown('### 0.4 项目开发流程图')
     image = load_image('data/source/flow_chart.png')
     st.image(image, use_column_width=True)
 
+    st.markdown('***')
     st.markdown('### 0.5 项目实用技术')
     tech_list = pd.DataFrame(
         {'模块': ['数据爬取', '数据存储', '数据预处理', '隐含信息挖掘', '情感分析', '评论分词', '词云与语义网络', 'LDA主题模型', '模型评估与诊断', '可视化'],
@@ -173,6 +178,7 @@ if section == '0. 项目介绍与规划':
          })
     st.table(tech_list)
 
+    st.markdown('***')
     st.markdown('### 0.6 遇到的问题以及解决方法')
     st.markdown(' - 多人使用git版本管理中遇到文件冲突的问题')
     st.markdown('解决办法：从网上查找资料，回退版本')
@@ -194,10 +200,12 @@ if section == '0. 项目介绍与规划':
     st.markdown('解决办法：Negative的标签数据量较少，减少主题数量，重新建模')
     st.markdown(' - 项目部署到远端服务器后无法对html文件在浏览器中访问')
     st.markdown('解决办法：将文件部署到github.io上可以直接使用url对资源访问')
+    st.markdown('***')
 
-if section == '1. 数据采集与抽取':
+if section == '1⃣️ 数据采集与抽取':
     st.markdown('## 1. 数据采集与抽取')
 
+    st.markdown('***')
     st.markdown('### 1.1 数据爬取')
     st.markdown(' - 该项目在京东商城爬取美的热水器品牌原始数据，共计 {} 条'.format(len(data_origin)))
     st.markdown(' - 数据框（下方展示）中包含\n'
@@ -242,6 +250,7 @@ if section == '1. 数据采集与抽取':
     '''
     st.code(code_webcrawler)
 
+    st.markdown('***')
     st.markdown('### 1.2 数据存储（MongoDB）')
     code_save_mongo = '''
     def save_to_mongo(data):
@@ -259,16 +268,19 @@ if section == '1. 数据采集与抽取':
     '''
     st.code(code_save_mongo)
 
+    st.markdown('***')
     st.markdown('### 1.3 评论抽取')
     st.markdown('从原始中抽取评论数据（下方展示）')
     if st.checkbox('Show origin comments'):
         data_origin = load_data_origin()
         comments_origin = pd.DataFrame(data_origin.comment)
         st.dataframe(comments_origin, 500, 400)
+    st.markdown('***')
 
-if section == '2. 数据预处理与探索':
+if section == '2⃣️ 数据预处理与探索':
     st.markdown('## 2. 数据预处理与探索')
 
+    st.markdown('***')
     st.markdown('### 2.1 数据去重')
     st.markdown('评论数据进行去重后，共计 {} 条'.format(len(data_deldup)))
     if st.checkbox('Show delete duplicate comments'):
@@ -287,6 +299,7 @@ if section == '2. 数据预处理与探索':
     '''
     st.code(code_deldup)
 
+    st.markdown('***')
     st.markdown('### 2.2 机械压缩与短句删除')
     st.markdown('评论数据进行机械压缩与短句删除后，共计 {} 条'.format(len(data_compress)))
     if st.checkbox('Show compressed comments'):
@@ -319,6 +332,7 @@ if section == '2. 数据预处理与探索':
     '''
     st.code(code_compress)
 
+    st.markdown('***')
     st.markdown('### 2.3 隐含信息挖掘 -- 标签评论的获取')
     st.markdown('观察评论数据，发现部分评论数据有固定的主题标签，将其爬取下来进行商品某方面特征定向分析。')
     if st.checkbox('Show tag comments'):
@@ -349,6 +363,7 @@ if section == '2. 数据预处理与探索':
     '''
     st.code(code_extract_tag)
 
+    st.markdown('***')
     st.markdown('### 2.4 数据特征探索')
     st.markdown('#### 2.4.1 美的热水器商品评论的评分分布图')
     score = pd.DataFrame(data_compress.score.value_counts())
@@ -372,11 +387,12 @@ if section == '2. 数据预处理与探索':
         image = load_image('data/source/highscore_keywords.png')
         st.image(image, use_column_width=True)
     st.markdown('参考文献：[SparkClouds: Visualizing Trends in Tag Clouds](https://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=5613457)')
+    st.markdown('***')
 
-
-if section == '3. 自训练情感分析模型':
+if section == '3⃣️ 自训练情感分析模型':
     st.markdown('## 3. 自训练情感分析模型')
 
+    st.markdown('***')
     st.markdown('### 3.1 模型神经网络结构图网络')
 
     b1 = st.checkbox('显示结构图')
@@ -391,6 +407,7 @@ if section == '3. 自训练情感分析模型':
             unsafe_allow_html=True,
         )
 
+    st.markdown('***')
     st.markdown('### 3.2 模型训练Loss曲线')
 
     b2 = st.checkbox('显示Loss曲线')
@@ -414,6 +431,7 @@ if section == '3. 自训练情感分析模型':
     st.table(df)
     # st.bar_chart(df.T)
 
+    st.markdown('***')
     st.markdown('### 3.4 情感分析结果在评论评分中的分布')
     score_1 = data_comment[data_comment.score == 1].sentiment.value_counts()
     score_2 = data_comment[data_comment.score == 2].sentiment.value_counts()
@@ -432,19 +450,26 @@ if section == '3. 自训练情感分析模型':
     # st.pyplot()
     score[0] = score[0] * -1
     st.bar_chart(score, width=100, use_container_width=True)
+    st.markdown('***')
 
-if section == '4. 评论分词与改进':
+if section == '4⃣️ 评论分词与改进':
     st.markdown('## 4. 评论分词与改进')
+
+    st.markdown('***')
     st.markdown('### 4.1 总体评论分词')
     if st.checkbox('展示总体积极评论分词'):
         st.dataframe(pos_comment)
     if st.checkbox('展示总体消极评论分词'):
         st.dataframe(neg_comment)
+
+    st.markdown('***')
     st.markdown('### 4.2 标签评论分词')
     if st.checkbox('展示标签积极评论分词'):
         st.json(tag_comments_pos)
     if st.checkbox('展示标签消极评论分词'):
         st.json(tag_comments_neg)
+
+    st.markdown('***')
     st.markdown('### 4.3 评论分词改进')
     code_cut_word = '''
     def str_cut(comment, stop_words):
@@ -463,9 +488,12 @@ if section == '4. 评论分词与改进':
     return object_list
     '''
     st.code(code_cut_word)
+    st.markdown('***')
 
-if section == '5. 词云与语义网络构建':
+if section == '5⃣️ 词云与语义网络构建':
     st.markdown('## 5. 词云与语义网络构建')
+
+    st.markdown('***')
     st.markdown('### 5.1 美的热水器商品评论（{}）的词云'.format(tag))
     image = load_image('data/word_cloud/{}_pos.jpg'.format(tag))
     st.write('')
@@ -475,6 +503,7 @@ if section == '5. 词云与语义网络构建':
     st.image(image, caption='{} 消极情感词云'.format(tag), width=500)
     st.write('')
 
+    st.markdown('***')
     st.markdown('### 5.2 美的热水器商品评论（{}）的语义网络'.format(tag))
     st.write('')
     image = load_image('data/word_net/{}_pos/net.jpg'.format(tag))
@@ -483,16 +512,20 @@ if section == '5. 词云与语义网络构建':
     image = load_image('data/word_net/{}_neg/net.jpg'.format(tag))
     st.image(image, caption='{} 消极情感语义网络'.format(tag), use_column_width=True)
     st.write('')
+    st.markdown('***')
 
-if section == '6. LDA主题模型构建':
+if section == '6⃣️ LDA主题模型构建':
     st.markdown('## 6. LDA主题模型构建')
 
+    st.markdown('***')
     st.markdown('### 6.1 LDA关键字与主题提取\n')
-    if st.button('正面评论LDA结果展示'):
-        login(u'lda_{}_{}_pos.html'.format(tag, topic_number))
+    # if st.button('正面评论LDA结果展示'):
+    #     login(u'lda_{}_{}_pos.html'.format(tag, topic_number))
+    st.markdown('### - [正面评论LDA结果展示](https://dyf-2316.github.io/LDA_Results/lda_{}_{}_pos.html)'.format(tag, topic_number))
 
-    if st.button('负面评论LDA结果展示'):
-        login(u'lda_{}_{}_neg.html'.format(tag, topic_number))
+    # if st.button('负面评论LDA结果展示'):
+    #     login(u'lda_{}_{}_neg.html'.format(tag, topic_number))
+    st.markdown('### - [负面评论LDA结果展示](https://dyf-2316.github.io/LDA_Results/lda_{}_{}_neg.html)'.format(tag, topic_number))
 
     code_LDA = '''
     def LDA(data, components, htmlfile=None):
@@ -525,6 +558,7 @@ if section == '6. LDA主题模型构建':
     '''
     st.code(code_LDA)
 
+    st.markdown('***')
     st.markdown('### 6.2 LDA模型优化')
     st.markdown('用 cv coherence 来度量主题的连贯性，以此来选择出最优的 主题数 作为超参数')
 
@@ -539,7 +573,8 @@ if section == '6. LDA主题模型构建':
     data_cv_neg = data_cv_neg.T
     data_cv_neg = (data_cv_neg['cv'] - data_cv_neg['cv'].min()) / (data_cv_neg['cv'].max() - data_cv_neg['cv'].min())
     st.line_chart(data_cv_neg)
+    st.markdown('***')
 
 
-if section == '7. 模型评估与优化':
+if section == '7⃣️ 模型评估与优化':
     pass
